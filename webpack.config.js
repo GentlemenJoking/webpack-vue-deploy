00f0d166c56b56d414d50fda161057a80c5d4cd1
GentlemenJoking <52685159+GentlemenJoking@users.noreply.github.com>
2020-04-05 21:15:02 +0800

    Add files via upload
    webpack 和 .vue 组件的基本配置

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack'); 
+const htmlWebpackPlugin = require('html-webpack-plugin'); 
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+   
+    entry: path.join(__dirname, './src/main.js'),
+    output:{
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        open:true, 
+        port: 3000, 
+        contentBase: 'src',
+        hot: true 
+    },
+    plugins: [ 
+        new webpack.HotModuleReplacementPlugin(),
+        new htmlWebpackPlugin({
+            template:path.join(__dirname, './src/index.html'),
+            filename: 'index.html'
+        }),
+        new VueLoaderPlugin()
+    ],
+    resolve: {
+        alias: { 
+            "vue$": "vue/dist/vue"
+        }
+    },
+    module: { 
+        rules: [ 
+        
+            {test: /\.css$/,use: ['style-loader', 'css-loader']},
+        
+            {test: /\.less$/,use: ['style-loader', 'css-loader','less-loader']},
+        
+            {test: /\.sass$/,use: ['style-loader', 'css-loader','sass-loader']},
+
+            {
+                test: /\.(jpg|png|gif|bmp|jpeg)$/,
+                use: 'url-loader?limit=7631&name=[hash:8]-[name].[ext]'
+            },
+
+            {test: /\.(ttf|eot|svg|woff|woff2)$/,use: 'url-loader'},
+
+            {test: /\.js$/,use: 'babel-loader', exclude: /node_modules/},
+
+            {test: /\.vue$/, use: 'vue-loader'}
+        ]},
+}
